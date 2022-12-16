24a1ab82be4ae8cab2bb3ad7245178135d531039
Marc Doerflinger <marc@doerf.li>
2022-12-16 08:07:32 +0000

    avoid duplicates

--- src/redux/slices/staking.ts
+++ src/redux/slices/staking.ts
@@ -30,7 +30,10 @@ export const stakingSlice = createSlice({
             state.step = action.payload;
         },
         add: (state, action: PayloadAction<BundleInfo>) => {
-            state.bundles.push(action.payload);
+            const hasBundle = state.bundles.find((bundle) => bundle.id === action.payload.id) !== undefined;
+            if (! hasBundle) {
+                state.bundles.push(action.payload);
+            }
         },
         reset: (state) => {
             state.bundles = [];
