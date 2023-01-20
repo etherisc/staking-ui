32912ff69322fd3f9062cae050858a4be069a155
Marc Doerflinger <marc@doerf.li>
2023-01-20 14:17:06 +0000

    fix sorting of stakes table

--- src/components/bundle_stakes/bundle_stakes.tsx
+++ src/components/bundle_stakes/bundle_stakes.tsx
@@ -1,5 +1,5 @@
 import { LinearProgress, useTheme } from "@mui/material";
-import { DataGrid, GridColDef, GridRenderCellParams, GridValueFormatterParams, GridValueGetterParams } from "@mui/x-data-grid";
+import { DataGrid, GridColDef, gridNumberComparator, GridRenderCellParams, GridValueFormatterParams, GridValueGetterParams } from "@mui/x-data-grid";
 import { BigNumber } from "ethers";
 import { useTranslation } from "next-i18next";
 import { useEffect, useState } from "react";
@@ -13,6 +13,7 @@ import Address from "../address";
 import { StakeUsage } from "../../utils/types";
 import { formatAmount } from "../../utils/format";
 import StakeUsageIndicator from "./stake_usage_indicator";
+import { bigNumberComparator } from "../../utils/bignumber";
 
 interface BundleStakesProps {
     stakingApi: StakingApi;
@@ -74,12 +75,13 @@ export default function BundleStakes(props: BundleStakesProps) {
                 return (<>
                     <Address address={params.value![0]} iconColor="secondary.main" />
                 </>);
-            }
+            },
+            sortComparator: (v1: [string, string], v2: [string, string]) => v1[0].localeCompare(v2[0]),
         },
         { 
             field: 'bundleId', headerName: t('table.header.bundleId'), flex: 0.5,
             valueGetter: (params: GridValueGetterParams<any, BundleInfo>) => [ params.row.bundleId, params.row.bundleName ],
-            renderCell: (params: GridRenderCellParams<[string, string]>) => {
+            renderCell: (params: GridRenderCellParams<[number, string]>) => {
                 if (params.value![1] === undefined || params.value![1] === null || params.value![1] === '') {
                     return (<>{params.value![0]}</>);
                 }
@@ -88,25 +90,37 @@ export default function BundleStakes(props: BundleStakesProps) {
                         {params.value![1]}
                     </WithTooltip>
                 </>);
+            },
+            sortComparator: (v1: [number, string], v2: [number, string]) => {
+                if (v1[1] !== '' && v2[1] !== '') {
+                    return v1[1].localeCompare(v2[1]);
+                } else if (v1[1] !== '') {
+                    return v1[1].localeCompare(v2[0].toString());
+                } else if (v2[1] !== '') {
+                    return v1[0].toString().localeCompare(v2[1]);
+                } else {
+                    return v1[0] - v2[0];
+                }
             }
         },
         { 
             field: 'myStakedAmount', headerName: t('table.header.myStakedAmount'), flex: 0.7,
-            valueGetter: (params: GridValueGetterParams<any, BundleInfo>) => [ params.row.myStakedAmount, params.row.stakedAmount ],
-            renderCell: (params: GridRenderCellParams<[string, string]>) => (<>
-                    <WithTooltip tooltipText={`${t('staked_amount_total')} ${formatAmount(BigNumber.from(params.value![1]), currency, currencyDecimals)}`}>
-                        {formatAmount(BigNumber.from(params.value![0]), currency, currencyDecimals)}
+            valueGetter: (params: GridValueGetterParams<any, BundleInfo>) => [ BigNumber.from(params.row.myStakedAmount), BigNumber.from(params.row.stakedAmount) ],
+            renderCell: (params: GridRenderCellParams<[BigNumber, BigNumber]>) => (<>
+                    <WithTooltip tooltipText={`${t('staked_amount_total')} ${formatAmount(params.value![1], currency, currencyDecimals)}`}>
+                        {formatAmount(params.value![0], currency, currencyDecimals)}
                     </WithTooltip>
-                </>)
+                </>),
+            sortComparator: (v1: [BigNumber], v2: [BigNumber]) => bigNumberComparator(v1[0], v2[0])
         },
         { 
             field: 'supportingAmount', headerName: t('table.header.myAllSupportingAmount'), flex: 1,
             valueGetter: (params: GridValueGetterParams<any, BundleInfo>) => 
-                [ params.row.mySupportingAmount, params.row.supportingAmount, params.row.supportingToken, params.row.supportingTokenDecimals ],
-            valueFormatter: (params: GridValueFormatterParams<[string, string, string, number]>) => {
-                // console.log("supportingAmount valueFormatter", params.value[0], ",", params.value[1], params.value[2], params.value[3]);
-                return formatAmountMineTotal(BigNumber.from(params.value[0]), BigNumber.from(params.value[1]), params.value[2], params.value[3]);
-            }
+                [ BigNumber.from(params.row.mySupportingAmount), BigNumber.from(params.row.supportingAmount), params.row.supportingToken, params.row.supportingTokenDecimals ],
+            valueFormatter: (params: GridValueFormatterParams<[BigNumber, BigNumber, string, number]>) => {
+                return formatAmountMineTotal(params.value[0], params.value[1], params.value[2], params.value[3]);
+            },
+            sortComparator: (v1: [BigNumber], v2: [BigNumber]) => bigNumberComparator(v1[0], v2[0])
         },
         { 
             field: 'state', headerName: t('table.header.state'), flex: 0.35,
@@ -132,7 +146,8 @@ export default function BundleStakes(props: BundleStakesProps) {
                             supportedToken={params.value![3]}
                             supportedTokenDecimals={params.value![4]}
                             />);
-            }
+            },
+            sortComparator: (v1: [StakeUsage], v2: [StakeUsage]) => gridNumberComparator(v1[0], v2[0])
         });
     }
 
@@ -141,6 +156,7 @@ export default function BundleStakes(props: BundleStakesProps) {
             field: 'actions', 
             headerName: t('table.header.actions'), 
             flex: 0.5,
+            sortable: false,
             valueGetter: (params: GridValueGetterParams<any, BundleInfo>) => 
                 params.row,
             renderCell: (params: GridRenderCellParams<BundleInfo>) => {
