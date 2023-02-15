fc3bfe2f9e6a35e2f70f10e2e965fd000708c3a7
Marc Doerflinger <marc@doerf.li>
2023-02-15 15:57:31 +0000

    show token symbol in approval info message

--- src/backend/erc20.ts
+++ src/backend/erc20.ts
@@ -1,9 +1,9 @@
 import { BigNumber, ContractReceipt, ContractTransaction, Signer } from "ethers";
-import { IERC20, IERC20__factory } from "../contracts/gif-interface";
+import { ERC20, ERC20__factory } from "../contracts/depeg-contracts";
 import { ApprovalFailedError } from "../utils/error";
 
-export function getErc20Token(address: string, signer: Signer): IERC20 {
-    return IERC20__factory.connect(address, signer);
+export function getErc20Token(address: string, signer: Signer): ERC20 {
+    return ERC20__factory.connect(address, signer);
 }
 
 /** Checks weather the given wallet has the expected balance */
@@ -32,14 +32,15 @@ export async function createDipApproval(
     const dipAddress = process.env.NEXT_PUBLIC_DIP_ADDRESS!;
     console.log(`creating treasury approval for ${amount} on token ${dipAddress}`);
     const dip = getErc20Token(dipAddress, signer);
+    const symbol = await dip.symbol();
     if (beforeApprovalCallback !== undefined) {
-        beforeApprovalCallback(recipient, "", amount); // TODO: currency symbol
+        beforeApprovalCallback(recipient, symbol, amount);
     }
     try {
         const tx = await dip.approve(recipient, amount, { gasLimit: 200000 });
         console.log("tx done", tx)
         if (beforeWaitCallback !== undefined) {
-            beforeWaitCallback(recipient, "", amount); // TODO: currency symbol
+            beforeWaitCallback(recipient, symbol, amount); 
         }
         const receipt = await tx.wait();
         console.log("wait done", receipt, tx)
