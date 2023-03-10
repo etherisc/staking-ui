92f66e2c9723ca0f7af77c30ce69c3976c1eab4a
Marc Doerflinger <marc@doerf.li>
2023-03-10 15:39:22 +0000

    only allow claiming rewards if unclaimed rewards available (#158)

--- src/components/show_bundle/bundle_actions.tsx
+++ src/components/show_bundle/bundle_actions.tsx
@@ -1,9 +1,11 @@
 import { Button, Grid } from "@mui/material";
+import { BigNumber } from "ethers";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { BundleInfo } from "../../backend/bundle_info";
 import { bundleSelected } from "../../redux/slices/staking";
+import { RootState } from "../../redux/store";
 
 interface BundleDetailsProps {
     bundle: BundleInfo;
@@ -14,13 +16,16 @@ export default function BundleActions(props: BundleDetailsProps) {
     const { t } = useTranslation('stakes');
     const dispatch = useDispatch();
     const router = useRouter();
+    const ownedNfts = useSelector((state: RootState) => state.stakes.ownedNfts);
 
     const bundle = props.bundle;
-    const isStakingAllowed = props.bundle.stakingSupported;
-    // TODO: my stake > 0
-    const isUnstakingAllowed = props.bundle.unstakingSupported;
-    // TODO: if unclaimed rewards === 0 -> disable button
-    const isClaimRewardsAllowed = props.bundle.myStakedNfsIds.length > 0;
+    const isStakingAllowed = bundle.stakingSupported;
+    const isUnstakingAllowed = 
+        bundle.unstakingSupported &&
+        // Check if there is at least one NFT that is staked
+        ownedNfts.filter(nft => bundle.myStakedNfsIds.includes(nft.nftId) && BigNumber.from(nft.stakedAmount).gt(0)).length > 0;
+    // any unclaimed rewards left
+    const isClaimRewardsAllowed = bundle.myStakedNfsIds.length > 0 && BigNumber.from(bundle.unclaimedReward).gt(0);
     
     function stake() {
         dispatch(bundleSelected(bundle))
