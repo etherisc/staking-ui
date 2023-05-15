0edbaff1f2ffe8e07fa2c107fa4cbabfcaaffb6c
Marc Doerflinger <marc@doerf.li>
2023-05-15 10:54:43 +0000

    footer icons

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,4 +1,4 @@
-import { Button, Typography, useTheme } from '@mui/material';
+import { Button, Link, Typography, useTheme } from '@mui/material';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import React from 'react';
@@ -9,9 +9,12 @@ import buildInfo from "../../version.json";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWallet } from '@fortawesome/free-solid-svg-icons';
 import { addTokenToWallet } from '../../utils/wallet';
+import { faDiscord, faGithub, faTelegram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import Image from 'next/image';
 
 export default function Footer() {
     const theme = useTheme();
+    const fontSize = '0.7rem';
 
     function addDipToWallet() {
         const symbol = process.env.NEXT_PUBLIC_FAUCET_SYMBOL ?? 'DIP';
@@ -44,7 +47,32 @@ export default function Footer() {
                         }} 
                         justifySelf="left"
                         >
-                        <Typography variant="body2" sx={{ fontSize: '10px', ml: 1 }}  color="palette.priary.dark">
+                        <Box component="span" sx={{ mr: 0.5, mt: -0.5, mb: -1 }} >
+                            <Link href="https://etherisc.com" target="_blank" rel="noreferrer">
+                                <Image src="/etherisc_logo_bird_blue.svg" alt="Etherisc logo" width={24} height={24} />
+                            </Link>
+                        </Box>
+                        <Link href="https://twitter.com/Etherisc/" target="_blank" rel="noreferrer">
+                            <Typography sx={{lineHeight: '16px', color: theme.palette.text.primary}} >
+                                <FontAwesomeIcon icon={faTwitter} className="fa" />
+                            </Typography>
+                        </Link>
+                        <Link href="https://t.me/etherisc_community" target="_blank" rel="noreferrer">
+                            <Typography sx={{lineHeight: '16px', color: theme.palette.text.primary}} >
+                                <FontAwesomeIcon icon={faTelegram} className="fa" />
+                            </Typography>
+                        </Link>
+                        <Link href="https://discord.gg/cVsgakVG4R" target="_blank" rel="noreferrer">
+                            <Typography sx={{lineHeight: '16px', color: theme.palette.text.primary}} >
+                                <FontAwesomeIcon icon={faDiscord} className="fa" />
+                            </Typography>
+                        </Link>
+                        <Link href="https://github.com/etherisc/" target="_blank" rel="noreferrer">
+                            <Typography sx={{lineHeight: '16px', color: theme.palette.text.primary}} >
+                                <FontAwesomeIcon icon={faGithub} className="fa" />
+                            </Typography>
+                        </Link>
+                        <Typography variant="body2" sx={{ fontSize: fontSize, ml: 1, verticalAlign: 'middle' }} color={theme.palette.text.primary}>
                             {buildInfo.name} v{buildInfo.version} ({buildInfo.date})
                         </Typography>
                     </Box>
