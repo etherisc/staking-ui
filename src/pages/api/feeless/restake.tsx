991f778b77a274fbadabdca49c72f6c47f623bfc
Marc Doerflinger <marc@doerf.li>
2023-08-11 14:50:52 +0000

    remove todos

--- src/pages/api/feeless/restake.tsx
+++ src/pages/api/feeless/restake.tsx
@@ -14,31 +14,12 @@ export default async function handler(
 ) {
     if (process.env.NEXT_PUBLIC_FEATURE_GASLESS_TRANSACTION !== 'true') {
         res.status(404).send('Unsupported feature');
-    // TODO: remove?
-    // } else if (req.method === 'GET') {
-    //     await handleGet(req, res);
     } else if (req.method === 'POST') {
         await handlePost(req, res);
     } else {
         res.status(405).send('Only POST requests allowed');
     }
 }
-
-// TODO: remove ?
-// async function handleGet(req: NextApiRequest, res: NextApiResponse<Array<PendingApplication>>) {
-//     console.log("GET request to /api/restake");
-//     // don't cache this request
-//     res.setHeader('Cache-Control', 'no-store');
-//     const address = req.query.address as string;
-//     if (!address) {
-//         res.status(400).send([]);
-//         return; 
-//     }
-//     const pendingTxRepo = await getPendingApplicationRepository();
-//     const pendingPolicyHolderTransactions = await pendingTxRepo.search().where("policyHolder").equals(address).return.all();
-//     console.log("pendingPolicyHolderTransactions", pendingPolicyHolderTransactions.length);
-//     res.status(200).json(pendingPolicyHolderTransactions.map(tx => tx as any as PendingApplication));
-// }
 
 async function handlePost(req: NextApiRequest, res: NextApiResponse) {
     console.log("POST request to /api/restake");
